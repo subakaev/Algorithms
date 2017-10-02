e4d7ab729652566381f7a02db05b3cc6b7c88aaa
Sergey Bakaev <bakaev@yandex.ru>
2017-10-02 22:46:41 +0300

    Добавил описание к Bogosort

--- Algorithms.Sort/Bogosort.cs
+++ Algorithms.Sort/Bogosort.cs
@@ -4,6 +4,11 @@ using System.ComponentModel;
 
 namespace Algorithms.Sort
 {
+    /// <summary>
+    /// Наивный метод сортировки. 
+    /// Работает по принципу: проверяем - если массив не отсортирован, то перемешиваем данные и снова проверяем
+    /// Крайне неэффективный и нестабильный метод. Плохо работает уже при количестве элементов > 5
+    /// </summary>
     public class Bogosort<T> : ISort<T> where T : IComparable<T>
     {
         public T[] Sort(T[] array, ListSortDirection direction) {

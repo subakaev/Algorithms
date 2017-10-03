48c6e89095e204807eec95445e83379348123ae9
Sergey Bakaev <bakaev@yandex.ru>
2017-10-03 22:03:18 +0300

    Тесты для ArrayGenerator

---
+++ Algorithms.Common.Tests/ArrayGeneratorTests.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Linq;
+using Algorithms.Common.ItemGenerators;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+
+namespace Algorithms.Common.Tests
+{
+    [TestClass]
+    public class ArrayGeneratorTests
+    {
+        [TestMethod]
+        public void generate_generics_test() {
+            var moq = new Mock<IArrayItemGenerator<IComparable<object>>>();
+
+            moq.Setup(m => m.GenerateNext()).Returns(It.IsAny<IComparable<object>>());
+
+            var arr = ArrayGenerator.Generate(10, moq.Object);
+
+            Assert.AreEqual(10, arr.Length);
+        }
+
+        [TestMethod]
+        public void generate_int_unique_test() {
+            var arr = ArrayGenerator.Generate(3, false, 1, 4);
+
+            Assert.IsTrue(arr.Contains(1));
+            Assert.IsTrue(arr.Contains(2));
+            Assert.IsTrue(arr.Contains(3));
+        }
+
+        [TestMethod]
+        public void generate_int_unique_exception_test() {
+            Assert.ThrowsException<InvalidOperationException>(() => ArrayGenerator.Generate(3, false, 1, 3));
+        }
+
+        [TestMethod]
+        public void generate_int_not_unique_test() {
+            var arr = ArrayGenerator.Generate(3, true, 1, 1);
+
+            Assert.AreEqual(1, arr[0]);
+            Assert.AreEqual(1, arr[1]);
+            Assert.AreEqual(1, arr[2]);
+        }
+    }
+}
---
+++ Algorithms.Common.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Algorithms.Common.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Algorithms.Common.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("0a623ddc-29fb-4c38-9dfe-3127266a8366")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]

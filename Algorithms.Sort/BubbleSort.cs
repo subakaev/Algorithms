b2cfc5dbca595d81f3f4f79f957ef7e828b16833
Sergey Bakaev <bakaev@yandex.ru>
2017-10-09 22:30:15 +0300

    Простой визуализатор пузырьковой сортировки

--- Algorithms.Sort/BubbleSort.cs
+++ Algorithms.Sort/BubbleSort.cs
@@ -6,27 +6,21 @@ namespace Algorithms.Sort
 {
     public class BubbleSort<T> : ISort<T> where T : IComparable
     {
+        private readonly Action<int> afterSwapAction = i => { };
+
+        public BubbleSort() { }
+
+        public BubbleSort(Action<int> afterSwapAction) {
+            this.afterSwapAction = afterSwapAction;
+        }
+
         public T[] Sort(T[] array, ListSortDirection direction) {
             for (var i = 0; i < array.Length; i++) {
                 var isBreak = true;
 
                 for (var j = 0; j < array.Length - i - 1; j++) {
-                    switch (direction) {
-                        case ListSortDirection.Ascending:
-                            if (array[j].CompareTo(array[j + 1]) > 0) {
-                                array.Swap(j, j + 1);
-                                isBreak = false;
-                            }
-                            break;
-                        case ListSortDirection.Descending:
-                            if (array[j].CompareTo(array[j + 1]) < 0) {
-                                array.Swap(j, j + 1);
-                                isBreak = false;
-                            }
-                            break;
-                        default:
-                            throw new ArgumentOutOfRangeException(nameof(direction), direction, null);
-                    }
+                    if (TrySwapElements(array, j, j + 1, direction))
+                        isBreak = false;
                 }
 
                 if (isBreak)
@@ -34,6 +28,30 @@ namespace Algorithms.Sort
             }
 
             return array;
+        }
+
+        protected bool TrySwapElements(T[] array, int firstIndex, int secondIndex, ListSortDirection direction) {
+            if (CanSwapElements(array[firstIndex], array[secondIndex], direction)) {
+                array.Swap(firstIndex, secondIndex);
+
+                afterSwapAction(secondIndex);
+
+                return true;
+            }
+
+            return false;
+        }
+
+        private bool CanSwapElements(T first, T second, ListSortDirection direction) {
+            switch (direction)
+            {
+                case ListSortDirection.Ascending:
+                    return first.CompareTo(second) > 0;
+                case ListSortDirection.Descending:
+                    return first.CompareTo(second) < 0;
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(direction), direction, null);
+            }
         }
     }
 }
---
+++ Wpf.Gui/GraphControl.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.ComponentModel;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media;
+using System.Windows.Threading;
+using Algorithms.Common;
+using Algorithms.Sort;
+
+namespace Wpf.Gui
+{
+    class GraphControl : FrameworkElement
+    {
+        private int[] array;
+
+        private int maxItem;
+
+        private int selectedIndex;
+
+        public GraphControl() {
+            array = ArrayGenerator.Generate(150, false, 1, 151);
+
+            maxItem = array.Max();
+        }
+
+        public void Start() {
+            var t = new BubbleSort<int>(ProgressAction);
+
+            Task.Factory.StartNew(() => {
+                t.Sort(array, ListSortDirection.Ascending);
+            });
+        }
+
+        private void ProgressAction(int index) {
+            selectedIndex = index;
+            Dispatcher.BeginInvoke(DispatcherPriority.Normal, new Action(InvalidateVisual));
+            Thread.Sleep(10);
+        }
+
+        protected override void OnRender(DrawingContext context) {
+            context.DrawRectangle(Brushes.White, new Pen(Brushes.Black, 1), new Rect(new Size(ActualWidth, ActualHeight)));
+
+            var columnWidth = ActualWidth / (array.Length * 3 + 1);
+
+            var height = ActualHeight / (maxItem * 1.2);
+
+            var left = columnWidth;
+
+            for (var i = 0; i < array.Length; i++) {
+                context.DrawRectangle(i != selectedIndex ? Brushes.DodgerBlue : Brushes.Red, new Pen(Brushes.Black, 1), new Rect(left, ActualHeight - height * array[i], columnWidth * 2, ActualHeight));
+
+                left += columnWidth * 3;
+            }
+        }
+    }
+}
